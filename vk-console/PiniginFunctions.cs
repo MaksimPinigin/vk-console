dfb9ea4e841420a00309e8d08bac21b84c760d3f
MaksimPinigin <maksim@pinig.in>
2017-03-15 01:15:57 +0700

    Added PiniginFunctions.cs

---
+++ vk-console/PiniginFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vk_console
+{
+    class PiniginFunctions
+    {
+        public static void WriteColor(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+
+        public static void WriteColorLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static string ReadColorLine(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            string return_string = Console.ReadLine();
+            Console.ResetColor();
+            return return_string;
+        }
+    }
+}
