d20a2518a9b4e2aa1c83261f772af967d8f3b35e
Maksim Pinigin <maksim@pinig.in>
2017-05-25 21:35:18 +0700

    ConfigFile.cs: Improved GenerateConfigFile()

--- vk-console/ConfigFile.cs
+++ vk-console/ConfigFile.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace vk_console
 {
@@ -21,23 +22,37 @@ namespace vk_console
         public static JObject Parse()
         {
             CheckConfigFile();
-            return JObject.Parse(File.ReadAllText("config.json"));
+            var config_file = JObject.Parse(File.ReadAllText("config.json"));
+            return config_file;
         }
 
         public static void GenerateConfigFile()
         {
-            PiniginFunctions.WriteColorLine("WARNING: config.json not found!", ConsoleColor.Yellow);
+            //PiniginFunctions.WriteColorLine("WARNING: config.json not found!", ConsoleColor.Yellow);
             Console.WriteLine("===== WELCOME TO PINIGIN VK CONSOLE =====");
             Console.Write("Please, enter your id: "); string user_id = Console.ReadLine();
             PiniginFunctions.WriteColorLine("TIP: To get token authorize here: https://vk.cc/6n64Jy", ConsoleColor.Cyan);
             Console.Write("Enter your token: "); string access_token = Console.ReadLine();
-            var user = VkApi.GetUser(access_token, Int32.Parse(user_id), "id,first_name,last_name,deactivated,hidden");
-            //Console.WriteLine("Hello, " + user["first_name"] + "!");
-            string config = "{\"user_id\":"+ user_id + ",\"access_token\":\"" + access_token + "\"}";
-            File.WriteAllText("config.json",config);
-            PiniginFunctions.WriteColorLine("config.json created", ConsoleColor.Green);
-            Console.WriteLine("Please restart program");
-            Environment.Exit(0);
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://api.vk.com");
+            var request = new RestRequest();
+            request.Resource = $"/method/users.get?&access_token={access_token}&v=5.64&lang=en";
+            IRestResponse response = client.Execute(request);
+            var user_info = JObject.Parse(response.Content);
+            if((string)user_info["response"][0]["id"] == user_id)
+            {
+                //Console.WriteLine("Hello, " + user["first_name"] + "!");
+                string config = "{\"user_id\":" + user_id + ",\"access_token\":\"" + access_token + "\"}";
+                File.WriteAllText("config.json", config);
+                File.SetAttributes("config.json", FileAttributes.ReadOnly);
+                PiniginFunctions.WriteColorLine("config.json created", ConsoleColor.Green);
+                Console.WriteLine("Please restart program");
+                Environment.Exit(0);
+            }
+            else
+            {
+                PiniginFunctions.WriteColorLine("Token or id do not match", ConsoleColor.Red);
+            }
         }
     }
 }
