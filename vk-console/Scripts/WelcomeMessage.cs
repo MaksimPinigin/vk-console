c0b77537e3cfa966f4bef30a086d29c51153998d
Maksim Pinigin <maksim@pinig.in>
2017-05-25 17:56:21 +0700

    Added script WelcomeMessage.cs

---
+++ vk-console/Scripts/WelcomeMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vk_console.Scripts
+{
+    class WelcomeMessage
+    {
+        public static void Execute()
+        {
+            var config = ConfigFile.Parse();
+            var user = VkApi.GetUser((string)config["access_token"], (int)config["user_id"], "first_name,last_name");
+            Console.WriteLine("Hello, " + user["first_name"] + "!");
+        }
+    }
+}
